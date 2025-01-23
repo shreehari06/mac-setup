f008cbf5e1039cbc34c45f7dade2e55122e4f27e
Shreehari Thakral <shreehari_thakral@mckinsey.com>
2025-01-23 20:17:04 +0530

    feat: pull user info from file

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-Open ~/.zshrc, find the line that sets ZSH_THEME, and change its value to "powerlevel10k/powerlevel10k".
+# Open ~/.zshrc, find the line that sets ZSH_THEME, and change its value to "powerlevel10k/powerlevel10k".
 sed -i '' 's/^ZSH_THEME=.*$/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
 
 sudo chown -R $(whoami) /usr/local/bin
@@ -38,15 +38,15 @@ fi
 source ~/.zshrc
 
 # configure git
-git config --global user.name "Shreehari Thakral"
-git config --global user.email "shreehari.thakral06@gmail.com"
+git config --global user.name "$GIT_USERNAME"
+git config --global user.email "$GIT_EMAIL"
 
 # ssh
-ssh-keygen -t ed25519 -C "shreehari.thakral@gmail.com"
+ssh-keygen -t ed25519 -C "$GIT_EMAIL"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 pbcopy < ~/.ssh/id_ed25519.pub
-echo "SSH key copied to clipboard. Please add it to your GitHub account."
+echo "SSH key copied to clipboard. Please add it to your GitHub account - https://github.com/settings/keys"
 read -p "Press enter to continue"
 
 
